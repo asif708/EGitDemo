d78d2296623a3314485bb1bedcdb1fe9bd9b4cbb
asif708 <asif_immanad@persistent.co.in>
2020-04-12 21:02:24 +0530

    myMethod By user2 and made better by user 1

--- src/com/asif/HelloEgitDemo.java
+++ src/com/asif/HelloEgitDemo.java
@@ -2,10 +2,13 @@ package com.asif;
 
 public class HelloEgitDemo {
 
-	public static void main(String[] args) {
+	static void myMethod() {
 		System.out.println("This is 1st change");
 		System.out.println("Hello There Demo Of Git PlugIn");
-		
-		
+	}
+
+	public static void main(String[] args) {
+
+		myMethod();
 	}
 }
